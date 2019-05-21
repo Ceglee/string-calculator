5e53c4f5d061fe9b0582178bbe813879b4d6cdbf
CegLee <ceglarzp@gmail.com>
2019-05-21 22:56:57 +0100

    1. Create a simple String calculator with a method int Add(string numbers)

--- src/main/java/com/pc/StringCalculator.java
+++ src/main/java/com/pc/StringCalculator.java
@@ -3,6 +3,15 @@ package com.pc;
 public class StringCalculator {
 
     public int add(String numbers) {
-        return 0;
+        var sum = 0;
+        if (numbers != null && !numbers.isEmpty()) {
+            var partialNumbers = numbers.split(",");
+            if (partialNumbers.length == 2) {
+                sum = Integer.parseInt(partialNumbers[0]) + Integer.parseInt(partialNumbers[1]);
+            } else {
+                sum = Integer.parseInt(partialNumbers[0]);
+            }
+        }
+        return sum;
     }
 }
--- src/test/java/com/pc/StringCalculatorTest.java
+++ src/test/java/com/pc/StringCalculatorTest.java
@@ -15,7 +15,33 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void add_anyNumbersParameter() {
-        assertEquals("Should return 0", 0, stringCalculator.add(""));
+    public void add_argumentIsNull_returnZero() {
+        var expected = 0;
+        assertEquals("Should return " + expected, expected, stringCalculator.add(""));
+    }
+
+    @Test
+    public void add_argumentIsEmpty_returnZero() {
+        var expected = 0;
+        assertEquals("Should return " + expected, expected, stringCalculator.add(""));
+    }
+
+    @Test
+    public void add_argumentIsSingleNumber_returnThisNumber() {
+        var expected = 1;
+        assertEquals("Should return " + expected, expected, stringCalculator.add("1"));
+    }
+
+    @Test
+    public void add_argumentHasTwoNumbers_returnSum () {
+        var expected = 3;
+        assertEquals("Should return " + expected, expected, stringCalculator.add("1,2"));
+    }
+
+    @Test
+    public void add_argumentHasTwoNumbers_numberOrderDoesNotMatter () {
+        var expected = 5;
+        assertEquals("Should return " + expected, expected, stringCalculator.add("2,3"));
+        assertEquals("Should return " + expected, expected, stringCalculator.add("3,2"));
     }
 }
